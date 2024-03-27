89c7acc57b68693aa860c57dcc5984dd984a5868
Maria Kazakova <mariakazakova92@gmail.com>
2024-03-27 15:08:07 +0300

    added css styles to the FrontPage component

--- src/app/pages/frontPage/FrontPage.tsx
+++ src/app/pages/frontPage/FrontPage.tsx
@@ -1,8 +1,9 @@
 import {Introduction} from '@pages/frontPage/Introduction';
+import styles from './FrontPage.module.css'
 
 export const FrontPage = () => {
   return (
-    <>
+    <div className={styles.frontPage}>
       <Introduction/>
-    </>
+    </div>
 )}
--- src/app/pages/frontPage/Introduction.tsx
+++ src/app/pages/frontPage/Introduction.tsx
@@ -5,9 +5,9 @@ export const Introduction = () => {
     <section className={styles.introduction}>
       <div className={styles.introduction_name}>
         <h1>Ilya Kushlianski</h1>
-        <h2>Frontend developer</h2>
-        <h2>Backend developer</h2>
-        <h2>Mentor</h2>
+        <h2 className={styles.name_frontend}>Frontend developer</h2>
+        <h2 className={styles.name_backend}>Backend developer</h2>
+        <h2 className={styles.name_mentor}>Mentor</h2>
       </div>
 
       <div className={styles.introduction_photo}>
