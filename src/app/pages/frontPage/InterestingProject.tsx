630cdc39baf707ed1164597b4c96baacce01a937
Maria Kazakova <mariakazakova92@gmail.com>
2024-04-15 14:31:34 +0300

    added styles to the IntrestingProject component

--- src/app/pages/frontPage/InterestingProject.tsx
+++ src/app/pages/frontPage/InterestingProject.tsx
@@ -1,4 +1,5 @@
-import { Stack, Title, Text } from "@mantine/core";
+import { Stack, Title, Text, SimpleGrid } from "@mantine/core";
+import { theme } from "src/app/theme";
 
 export const InterestingProject = () => {
   return (
@@ -11,54 +12,61 @@ export const InterestingProject = () => {
       <Title order={2} style={{ "text-align": "center" }}>
         What project i am interested in
       </Title>
-      <div>
-        <Text
-          size="lg"
-          style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
-        >
-          Domain:
-        </Text>
-        <Text size="lg">
-          Any end-user-oriented project, preferably a project from scratch or
-          very little legacy code
-        </Text>
-      </div>
-      <div>
-        <Text
-          size="lg"
-          style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
-        >
-          Tech stack:
-        </Text>
-        <Text size="lg">
-          A full-stack React/Node.js project, Typescript, Amazon Web Services,
-          Postgres/MongoDB, GraphQL, microservices
-        </Text>
-      </div>
-      <div>
-        <Text
-          size="lg"
-          style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
-        >
-          Processes:
-        </Text>
-        <Text size="lg">
-          Well-established planning, grooming and estimation processes (or let
-          us set them up together!)
-        </Text>
-      </div>
-      <div>
-        <Text
-          size="lg"
-          style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
-        >
-          Testing & quality:
-        </Text>
-        <Text size="lg">
-          I would be glad to join a team boasting high engineering culture, who
-          value testing and make pipelines that allow sleeping well at night
-        </Text>
-      </div>
+      <SimpleGrid cols={{ base: 1, sm: 2 }}>
+        <div>
+          <Text
+            size="lg"
+            style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
+          >
+            Domain:
+          </Text>
+          <Text size="lg">
+            Any end-user-oriented project, preferably a project from scratch or
+            very little legacy code
+          </Text>
+        </div>
+        <div>Image</div>
+        <div>Image2</div>
+        <div>
+          <Text
+            size="lg"
+            style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
+          >
+            Tech stack:
+          </Text>
+          <Text size="lg">
+            A full-stack React/Node.js project, Typescript, Amazon Web Services,
+            Postgres/MongoDB, GraphQL, microservices
+          </Text>
+        </div>
+        <div>
+          <Text
+            size="lg"
+            style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
+          >
+            Processes:
+          </Text>
+          <Text size="lg">
+            Well-established planning, grooming and estimation processes (or let
+            us set them up together!)
+          </Text>
+        </div>
+        <div>Image3</div>
+        <div>Image4</div>
+        <div>
+          <Text
+            size="lg"
+            style={(theme) => ({ color: theme.colors.myOrangeColor[6] })}
+          >
+            Testing & quality:
+          </Text>
+          <Text size="lg">
+            I would be glad to join a team boasting high engineering culture,
+            who value testing and make pipelines that allow sleeping well at
+            night
+          </Text>
+        </div>
+      </SimpleGrid>
     </Stack>
   );
 };
--- src/app/pages/frontPage/MajorSkills.tsx
+++ src/app/pages/frontPage/MajorSkills.tsx
@@ -17,36 +17,36 @@ export const MajorSkills = () => {
       <Title order={2}>Major skills</Title>
       <SimpleGrid
         cols={{ base: 2, sm: 4 }}
-        spacing="xl"
-        verticalSpacing={{ xs: "lg", sm: "xxl" }}
+        spacing={{ base: "lg", sm: "xl" }}
+        verticalSpacing={{ base: "lg", xs: "lg", sm: "xxl" }}
       >
         <Image
           component={NextImage}
           src={javascript}
           alt={"JavaScriptImage"}
-          w={{ xs: 100, sm: 160 }}
-          h={{ xs: 100, sm: 160 }}
+          w={{ base: 100, xs: 100, sm: 160 }}
+          h={{ base: 100, xs: 100, sm: 160 }}
         />
         <Image
           component={NextImage}
           src={typescript}
           alt={"TypeScriptImage"}
-          w={{ xs: 100, sm: 160 }}
-          h={{ xs: 100, sm: 160 }}
+          w={{ base: 100, xs: 100, sm: 160 }}
+          h={{ base: 100, xs: 100, sm: 160 }}
         />
         <Image
           component={NextImage}
           src={react}
           alt={"ReactImage"}
-          w={{ xs: 100, sm: 160 }}
-          h={{ xs: 100, sm: 160 }}
+          w={{ base: 100, xs: 100, sm: 160 }}
+          h={{ base: 100, xs: 100, sm: 160 }}
         />
         <Image
           component={NextImage}
           src={node}
           alt={"NodeImage"}
-          w={{ xs: 100, sm: 160 }}
-          h={{ xs: 100, sm: 160 }}
+          w={{ base: 100, xs: 100, sm: 160 }}
+          h={{ base: 100, xs: 100, sm: 160 }}
         />
       </SimpleGrid>
       <ClickButton text={"Explore all my skills"} />
